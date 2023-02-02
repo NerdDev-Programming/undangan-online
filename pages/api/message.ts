df24a670125307280a434ba9c9afab01f305d17c
florensius daniel josef <capricondaniel@gmail.com>
2023-02-02 16:39:24 +0700

    api error handling

--- pages/api/message.ts
+++ pages/api/message.ts
@@ -6,34 +6,41 @@ export default async function handler(
 	res: NextApiResponse,
 ) {
 	if (req.method === "POST") {
-		const { id } = req.body;
+		try {
+			const { id } = req.body;
 
-		const cekData = await prisma.mempelai.findFirst({
-			where: {
-				id: id,
-			},
-		});
+			const cekData = await prisma.mempelai.findFirst({
+				where: {
+					id: id,
+				},
+			});
 
-		if (!cekData) {
-			return res.status(404).json({
-				message: "data tidak ada",
+			if (!cekData) {
+				return res.status(404).json({
+					message: "data tidak ada",
+				});
+			}
+
+			const data = await prisma.chat.findMany({
+				where: {
+					id_mempelai: id,
+				},
+				select: {
+					nama: true,
+					chat: true,
+				},
+			});
+
+			return res.status(200).json({
+				message: "successfully get data",
+				data: data,
+			});
+		} catch (err) {
+			console.log(`error get data : ${err.message}`);
+			return res.status(500).json({
+				message: "error while get data",
 			});
 		}
-
-		const data = await prisma.chat.findMany({
-			where: {
-				id_mempelai: id,
-			},
-			select: {
-				nama: true,
-				chat: true,
-			},
-		});
-
-		return res.status(200).json({
-			message: "successfully get data",
-			data: data,
-		});
 	} else {
 		return res.status(401).json({
 			message: "not supported yet",
--- pages/api/ucapan.ts
+++ pages/api/ucapan.ts
@@ -6,31 +6,38 @@ export default async function handler(
 	res: NextApiResponse,
 ) {
 	if (req.method === "POST") {
-		const { id, message, nama } = req.body;
+		try {
+			const { id, message, nama } = req.body;
 
-		const cekData = await prisma.mempelai.findFirst({
-			where: {
-				id: id,
-			},
-		});
+			const cekData = await prisma.mempelai.findFirst({
+				where: {
+					id: id,
+				},
+			});
 
-		if (!cekData) {
-			return res.status(404).json({
-				message: "data tidak ada",
+			if (!cekData) {
+				return res.status(404).json({
+					message: "data tidak ada",
+				});
+			}
+
+			await prisma.chat.create({
+				data: {
+					nama: nama as string,
+					chat: message as string,
+					id_mempelai: id as string,
+				},
+			});
+
+			return res.status(200).json({
+				message: "success",
+			});
+		} catch (err) {
+			console.log(`error send data : ${err.message}`);
+			return res.status(500).json({
+				message: "error while send data",
 			});
 		}
-
-		await prisma.chat.create({
-			data: {
-				nama: nama as string,
-				chat: message as string,
-				id_mempelai: id as string,
-			},
-		});
-
-		return res.status(200).json({
-			message: "success",
-		});
 	} else {
 		return res.status(401).json({
 			message: "not supported yet",
